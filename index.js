a42b2f8d5b7d050d5d1dc0d143e81d7a4d693c3d
saisuresh <saisureshv@adityauniversity.in>
2026-04-06 16:44:45 +0530

    yes

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const form =document.querySelector(".add");
 const incomeList=document.querySelector("ul.income-list");
 const expenseList=document.querySelector("ul.expense-list");
 let transactions=localStorage.getItem("transactions")!==null? JSON.parse(localStorage.getItem("transactions")): [];
+function addTransactionsDOM(id,source,amount,time){
+    if(amount>0){
+        incomeList.innerHTML+=` <li data-id="">
+                        <p>
+                            <span>Freelancing -Js project</span>
+                            <span id="time">12:45:38 AM 5/19/2022</span>
+                        </p>
+                        <span>$700</span>
+                        <i class="bi bi-trash delete"></i>
+                    </li> `;
+    }
+    else{
+        expenseList.innerHTML+=`<li>ABC</li>`;
+    }
+}
 function addTransaction(source, amount){
     const time = new Date();
 
@@ -15,6 +30,7 @@ function addTransaction(source, amount){
     transactions.push(transaction);
 
     localStorage.setItem("transactions", JSON.stringify(transactions)); 
+    addTransactionsDOM(id,source,amount,time);
 }
 form.addEventListener("submit",event=>{
     event.preventDefault();
